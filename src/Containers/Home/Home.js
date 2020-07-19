8f017b5af90635602aff62730d05bf7b02b00d7d
Bruno <bruno.webjr@gmail.com>
2020-07-19 00:06:10 -0300

    ...

---
+++ src/Containers/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Books from '../../Components/Books';
+
+export default function Home() {
+    return (
+      <div className="Home">
+          <Books />
+      </div>
+    )
+}
